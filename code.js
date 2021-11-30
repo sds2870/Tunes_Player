765f748bed19e20e9117601768d38f6fbca84b32
Steven Smith <sds2870@yahoo.com>
2021-11-30 00:22:26 -0600

    Musically committed

--- code.js
+++ code.js
@@ -11,3 +11,35 @@ songArtist.className = "artist_name";
 songArtist.innerHTML = "artist";
 musicPlayer.append(songTitle, songArtist);
 
+let diskCover = document.createElement("div");
+diskCover.className = "disk"
+
+let songSeeker = document.createElement("div");
+songSeeker.className = "song_seeker"
+
+let rangeFinder = document.createElement("input");
+rangeFinder.type = "range";
+rangeFinder.value = "0"
+rangeFinder.className = "seek_bar";
+
+let songTime = document.createElement("span")
+songTime.className = "current_time";
+songTime.innerHTML = "00:00";
+let songLength = document.createElement("span")
+songLength.className = "song_duration";
+songLength.innerHTML = "00:00"
+
+songSeeker.append(rangeFinder, songTime, songLength);
+musicPlayer.append(diskCover, songSeeker);
+
+let playerControls = document.createElement("div");
+playerControls.className = "controls";
+
+let buttonSelect = document.createElement("button");
+buttonSelect.className = "play_btn";
+let buttonSpanOne = document.createElement("span");
+let butonSpanTwo = document.createElement("span");
+buttonSelect.append(buttonSpanOne, butonSpanTwo);
+playerControls.append (buttonSelect);
+
+musicPlayer.append(playerControls);
